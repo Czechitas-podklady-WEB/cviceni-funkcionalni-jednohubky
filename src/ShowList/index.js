c615307459b6183ed8017ee5e5a9a5ba50fae5a4
Filip Jirsák <filip@jirsak.org>
2022-04-29 23:43:22 +0200

    Opravy kódu, stylování stránky

--- src/ShowList/index.js
+++ src/ShowList/index.js
@@ -10,10 +10,27 @@ const H2 = ({ text }) => {
   return element;
 }
 
+const Column = ({ title, content }) => {
+  const element = document.createElement('div');
+  element.classList.add('column')
+  element.appendChild(H2({ text: title }))
+
+  const body = document.createElement('div');
+  if (Array.isArray(content)) {
+    content.forEach(item => body.appendChild(item))
+  } else {
+    body.appendChild(content)
+  }
+
+  element.appendChild(body)
+  return element;
+}
+
 const List = ({ list }) => {
   if (list == null) {
     const emptyElement = document.createElement('div');
-    emptyElement.innerHTML = `<em>Nic tu není</em>`
+    emptyElement.classList.add('empty')
+    emptyElement.innerHTML = '❔'
     return emptyElement
 
   }
@@ -27,48 +44,67 @@ const List = ({ list }) => {
   return listElement;
 }
 
-const TrueOrFalse = ({ value }) => {
-  const element = document.createElement('div');
-  element.textContent = value ? '✔' : '❌'
-  return element;
+const Value = ({ title, value }) => {
+  const rootElement = document.createElement('div');
+  rootElement.classList.add('item')
+
+  const textElement = document.createElement('div');
+  textElement.textContent = title
+  rootElement.appendChild(textElement)
+
+  rootElement.appendChild(value)
+
+  return rootElement;
+}
+
+const TrueOrFalse = ({ title, value }) => {
+  const icon = document.createElement('span');
+  if (value == null) {
+    icon.textContent = '❔'
+  } else {
+    icon.textContent = value ? '✅' : '❌'
+  }
+
+
+  return Value({ title, value: icon })
+}
+
+const NumberValue = ({ title, value }) => {
+  const number = document.createElement('span');
+  if (value == null) {
+    number.textContent = '❔'
+  } else {
+    number.textContent = value.toLocaleString('cs-CZ', { maximumFractionDigits: 1 })
+  }
+
+  return Value({ title, value: number })
 }
 
 const mapPersonInfo = (list) => {
   if (list == null) {
     return null
   }
-  return list.map(item => `${item.name}, ${item.age}, ${item.gender}, `)
+  return list.map(item => `${item.name}, ${item.age}, ${item.gender}`)
 }
 
-export const ShowList = ({ title, list, maleOrFemale, otherGender, someMan, longNames, adultMale, prettyName, averageAge }) => {
-  const rootElement = document.createElement('div');
+export const ShowList = ({ list, maleOrFemale, otherGender, someWoman, longNames, adultMale, prettyNameAge, prettyNameGender, averageAge }) => {
 
-  rootElement.appendChild(H1({ text: title }))
+  const rootElement = document.createElement('section');
 
-  rootElement.appendChild(H2({ text: 'Celý seznam' }))
+  rootElement.appendChild(Column({ title: 'Výchozí seznam', content: List({ list: mapPersonInfo(list) }) }))
 
-  rootElement.appendChild(List({ list: mapPersonInfo(list) }))
-
-  rootElement.appendChild(H2({ text: 'Jen muži a ženy' }))
-  rootElement.appendChild(TrueOrFalse({ value: maleOrFemale(list) }))
-
-  rootElement.appendChild(H2({ titextle: 'Seznam obsahuje jiná pohlaví' }))
-  rootElement.appendChild(TrueOrFalse({ value: otherGender(list) }))
-
-  rootElement.appendChild(H2({ text: 'Seznam obsahuje nějakého muže' }))
-  rootElement.appendChild(TrueOrFalse({ value: someMan(list) }))
-
-  rootElement.appendChild(H2({ text: 'Jména delší než 4 znaky' }))
-  rootElement.appendChild(List({ list: mapPersonInfo(longNames(list)) }))
-
-  rootElement.appendChild(H2({ text: 'Dospělí muži' }))
-  rootElement.appendChild(List({ list: mapPersonInfo(adultMale(list)) }))
-
-  rootElement.appendChild(H2({ text: 'Hezké zobrazení jmen' }))
-  rootElement.appendChild(List({ list: mapPersonInfo(prettyName(list)) }))
-
-  rootElement.appendChild(H2({ text: 'Průměrný věk' }))
-  rootElement.appendChild(document.createTextNode(averageAge(list)))
+  rootElement.appendChild(Column({
+    title: 'Údaje', content: [
+      TrueOrFalse({ title: 'Seznam obsahuje nějakou ženu', value: someWoman(list) }),
+      TrueOrFalse({ title: 'Seznam obsahuje jen muže a ženy', value: maleOrFemale(list) }),
+      TrueOrFalse({ title: 'Seznam obsahuje jiná pohlaví', value: otherGender(list) }),
+      NumberValue({ title: 'Průměrný věk', value: averageAge(list) }),
+    ]
+  }))
+  rootElement.appendChild(Column({ title: 'Jména delší než 4 znaky', content: List({ list: mapPersonInfo(longNames(list)) }) }))
+  rootElement.appendChild(Column({ title: 'Dospělí muži', content: List({ list: mapPersonInfo(adultMale(list)) }) }))
+  rootElement.appendChild(Column({ title: 'Jméno a věk', content: List({ list: prettyNameAge(list) }) }))
+  rootElement.appendChild(Column({ title: 'Dospělí – jméno a pohlaví', content: List({ list: prettyNameGender(list) }) }))
 
   return rootElement;
 }
--- src/index.js
+++ src/index.js
@@ -1,94 +1,135 @@
-import { ShowList } from './ShowList';
 import './style.css';
+import { ShowList } from './ShowList';
 
 const app = document.getElementById('app');
 
-const maleOrFemale = (list) => {
-
+export const showPersons = (list) => {
+  app.appendChild(
+    ShowList({
+      list,
+      someWoman,
+      maleOrFemale,
+      otherGender,
+      longNames,
+      adultMale,
+      prettyNameAge,
+      prettyNameGender,
+      averageAge
+    })
+  )
 }
 
-const otherGender = (list) => {
+// Implementujte následující funkce
 
+/**
+ * Vrací příznak, zda seznam osob obsahuje nějakou ženu.
+ */
+const someWoman = (persons) => {
+  return persons.some((person) => person.gender === 'female')
 }
 
-const someMan = (list) => {
-
+/**
+ * Vrací příznak, zda seznam osob obsahuje jen muže nebo ženy.
+ */
+const maleOrFemale = (persons) => {
+  return persons.every((person) => person.gender === 'male' || person.gender === 'female')
 }
 
-const longNames = (list) => {
-
+/**
+ * Vrací příznak, zda seznam osob obsahuje jiné pohlaví než muži nebo ženy.
+ */
+const otherGender = (persons) => {
+  return persons.some((person) => person.gender !== 'male' && person.gender !== 'female')
 }
 
-const adultMale = (list) => {
-
+/**
+ * Vrací seznam osob, jejichž jméno je delší než 4 znaky
+ */
+const longNames = (persons) => {
+  return persons.filter((person) => person.name.length > 4)
 }
 
-const prettyName = (list) => {
-
+/**
+ * Vrací seznam dospělých mužů.
+ */
+const adultMale = (persons) => {
+  return persons.filter((person) => person.age > 17 && person.gender === 'male')
 }
 
-const averageAge = (list) => {
-
+/**
+ * Vrací seznam osob ve formátu "Jméno (věk)"
+ */
+const prettyNameAge = (persons) => {
+  return persons.map((person) => `${person.name} (${person.age})`)
 }
 
-app.appendChild(
-  ShowList({
-    title: 'Základní seznam',
-    list: [
-      { name: 'Petr', age: 16, gender: 'male' },
-      { name: 'Jana', age: 8, gender: 'female' },
-      { name: 'Pavel', age: 34, gender: 'male' },
-      { name: 'Zuzana', age: 41, gender: 'female' },
-      { name: 'Eva', age: 13, gender: 'female' },
-      { name: 'Adam', age: 22, gender: 'male' },
-      { name: 'Adam', age: 22, gender: 'male' },
-      { name: 'Onyx', age: 37, gender: 'intersex' },
-    ],
-    maleOrFemale,
-    otherGender,
-    someMan,
-    longNames,
-    adultMale,
-    prettyName,
-    averageAge
-  })
+/**
+ * Vrací seznam dospělých osob ve formátu "X Jméno" kde X je značka pohlaví: ♂, ♀, ○
+ */
+const prettyNameGender = (persons) => {
+  return persons
+    .filter((person) => person.age >= 18)
+    .map(person => {
+      let symbol = '○'
+      if (person.gender === 'female') {
+        symbol = '♀'
+      } else if (person.gender === 'male') {
+        symbol = '♂'
+      }
+      return {
+        name: person.name,
+        genderSymbol: symbol
+      }
+    })
+    .map((person) => `${person.genderSymbol} ${person.name}`)
+}
+
+/**
+ * Vrací průměrný věk osob. 
+ */
+const averageAge = (persons) => {
+  let sum = 0;
+  persons.forEach((person) => sum += person.age)
+  return sum / persons.length
+}
+
+// zobrazení seznamů
+showPersons(
+  [
+    { name: 'Petr', age: 16, gender: 'male' },
+    { name: 'Jana', age: 8, gender: 'female' },
+    { name: 'Pavel', age: 34, gender: 'male' },
+    { name: 'Zuzana', age: 41, gender: 'female' },
+    { name: 'Eva', age: 13, gender: 'female' },
+    { name: 'Adam', age: 22, gender: 'male' },
+    { name: 'Tomáš', age: 17, gender: 'male' },
+    { name: 'Onyx', age: 37, gender: 'intersex' },
+  ]
 )
 
-app.appendChild(
-  ShowList({
-    title: 'Pouze muži',
-    list: [
-      { name: 'Petr', age: 16, gender: 'male' },
-      { name: 'Pavel', age: 34, gender: 'male' },
-      { name: 'Adam', age: 22, gender: 'male' },
-      { name: 'Adam', age: 22, gender: 'male' }
-    ],
-    maleOrFemale,
-    otherGender,
-    someMan,
-    longNames,
-    adultMale,
-    prettyName,
-    averageAge
-  })
+showPersons(
+  [
+    { name: 'Jana', age: 8, gender: 'female' },
+    { name: 'Zuzana', age: 41, gender: 'female' },
+    { name: 'Eva', age: 13, gender: 'female' },
+  ]
 )
 
-app.appendChild(
-  ShowList({
-    title: 'Pouze ženy',
-    list: [
-      { name: 'Jana', age: 8, gender: 'female' },
-      { name: 'Zuzana', age: 41, gender: 'female' },
-      { name: 'Eva', age: 13, gender: 'female' },
-      { name: 'Adam', age: 22, gender: 'male' },
-      { name: 'Adam', age: 22, gender: 'male' },
-    ],
-    maleOrFemale,
-    otherGender,
-    someMan,
-    longNames,
-    adultMale,
-    prettyName,
-    averageAge
-  })
+showPersons(
+  [
+    { name: 'Anna', age: 16, gender: 'female' },
+    { name: 'Pavel', age: 34, gender: 'male' },
+    { name: 'Jiřina', age: 22, gender: 'female' },
+    { name: 'Pat', age: 9, gender: 'wood' },
+    { name: 'Mat', age: 9, gender: 'wood' }
+  ]
+)
+
+showPersons(
+  [
+    { name: 'Petr', age: 16, gender: 'male' },
+    { name: 'Pavel', age: 34, gender: 'male' },
+    { name: 'Adam', age: 22, gender: 'male' },
+    { name: 'Tomáš', age: 17, gender: 'male' }
+  ]
 )
